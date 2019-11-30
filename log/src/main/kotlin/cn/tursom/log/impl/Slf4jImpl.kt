87aa8816bdd7dc6b6002346a4dc3e7ef5a47e4f8
tursom <tursom@foxmail.com>
2019-11-30 10:35:04 +0800

    添加日志工具类

--- log/src/main/kotlin/cn/tursom/log/impl/Slf4jImpl.kt
+++ log/src/main/kotlin/cn/tursom/log/impl/Slf4jImpl.kt
@@ -1,12 +1,13 @@
 package cn.tursom.log.impl
 
 import cn.tursom.log.Slf4j
+import cn.tursom.log.TrySlf4j
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import kotlin.reflect.KClass
 import kotlin.reflect.jvm.jvmName
 
-class Slf4jImpl(name: String? = null) : Slf4j {
+class Slf4jImpl(name: String? = null) : Slf4j, TrySlf4j {
   constructor(clazz: Class<*>?) : this(clazz?.name)
   constructor(clazz: KClass<*>?) : this(clazz?.jvmName?.let {
     if (clazz.isCompanion) it.dropLast(10) else it
@@ -20,4 +21,7 @@ class Slf4jImpl(name: String? = null) : Slf4j {
   } else {
     throw NotImplementedError("")
   })
+
+  override val logger: Logger get() = log
+  override val sfl4j: Logger get() = log
 }
