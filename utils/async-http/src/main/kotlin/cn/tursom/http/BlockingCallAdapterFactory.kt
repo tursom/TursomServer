a1046bca98f9c4f1abeb3709de044d8c0752616b
tursom <tursom@foxmail.com>
2020-07-19 12:11:24 +0800

    update BlockingCallAdapterFactory

--- utils/async-http/src/main/kotlin/cn/tursom/http/BlockingCallAdapterFactory.kt
+++ utils/async-http/src/main/kotlin/cn/tursom/http/BlockingCallAdapterFactory.kt
@@ -4,17 +4,20 @@ import retrofit2.Call
 import retrofit2.CallAdapter
 import retrofit2.Retrofit
 import java.lang.reflect.Type
+import java.util.concurrent.CompletableFuture
 
 object BlockingCallAdapterFactory : CallAdapter.Factory() {
   override fun get(
     returnType: Type,
     annotations: Array<Annotation>,
     retrofit: Retrofit
-  ): CallAdapter<*, *>? {
+  ): CallAdapter<out Any?, out Any?>? {
+    if (getRawType(returnType) == Call::class.java) return null
+    if (getRawType(returnType) == CompletableFuture::class.java) return null
     if (annotations.any { it is retrofit2.SkipCallbackExecutor }) return null
-    return object : CallAdapter<Any, Any> {
+    return object : CallAdapter<Any?, Any?> {
       override fun responseType(): Type = returnType
-      override fun adapt(call: Call<Any>): Any? = call.execute().body()
+      override fun adapt(call: Call<Any?>): Any? = call.execute().body()
     }
   }
 }
