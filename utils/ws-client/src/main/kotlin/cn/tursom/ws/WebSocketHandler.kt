0de8e2eaf78b408e314414cd321d9fa55736d905
tursom <tursom@foxmail.com>
2021-01-20 15:08:51 +0800

    update

--- utils/ws-client/src/main/kotlin/cn/tursom/ws/WebSocketHandler.kt
+++ utils/ws-client/src/main/kotlin/cn/tursom/ws/WebSocketHandler.kt
@@ -1,9 +1,11 @@
 package cn.tursom.ws
 
 import cn.tursom.core.buffer.ByteBuffer
+import cn.tursom.core.toUTF8String
 import cn.tursom.utils.bytebuffer.NettyByteBuffer
 import io.netty.buffer.ByteBuf
 import io.netty.handler.codec.http.websocketx.BinaryWebSocketFrame
+import io.netty.handler.codec.http.websocketx.PingWebSocketFrame
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame
 
 interface WebSocketHandler {
@@ -30,5 +32,43 @@ interface WebSocketHandler {
 
   fun readMessage(client: WebSocketClient, msg: BinaryWebSocketFrame) {
     readMessage(client, msg.content())
+  }
+
+  fun readPing(client: WebSocketClient, msg: PingWebSocketFrame) {
+    readPing(client, msg.content())
+  }
+
+  fun readPing(client: WebSocketClient, msg: ByteBuf) {
+    readPing(client, NettyByteBuffer(msg))
+  }
+
+  fun readPing(client: WebSocketClient, msg: ByteBuffer) {
+    readPing(client, msg.getBytes())
+  }
+
+  fun readPing(client: WebSocketClient, msg: ByteArray) {
+    readPing(client, msg.toUTF8String())
+  }
+
+  fun readPing(client: WebSocketClient, msg: String) {
+  }
+
+  fun readPong(client: WebSocketClient, msg: PingWebSocketFrame) {
+    readPong(client, msg.content())
+  }
+
+  fun readPong(client: WebSocketClient, msg: ByteBuf) {
+    readPong(client, NettyByteBuffer(msg))
+  }
+
+  fun readPong(client: WebSocketClient, msg: ByteBuffer) {
+    readPong(client, msg.getBytes())
+  }
+
+  fun readPong(client: WebSocketClient, msg: ByteArray) {
+    readPong(client, msg.toUTF8String())
+  }
+
+  fun readPong(client: WebSocketClient, msg: String) {
   }
 }
