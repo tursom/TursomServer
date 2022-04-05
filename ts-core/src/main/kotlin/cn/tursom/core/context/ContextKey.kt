b8b63acb48760637b51305700fd5fe85c8c234b6
tursom <tursom@foxmail.com>
2022-04-05 20:26:24 +0800

    add Context

--- ts-core/src/main/kotlin/cn/tursom/core/context/ContextKey.kt
+++ ts-core/src/main/kotlin/cn/tursom/core/context/ContextKey.kt
@@ -3,4 +3,18 @@ package cn.tursom.core.context
 data class ContextKey<T>(
   val envId: Int,
   val id: Int,
-)
+) {
+  override fun hashCode(): Int {
+    return id
+  }
+
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (other !is ContextKey<*>) return false
+
+    if (envId != other.envId) return false
+    if (id != other.id) return false
+
+    return true
+  }
+}
